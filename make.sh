b22bcdbb7272d828b7cd2653040abe5b0a7aef86
Jose Abell <ja.abell@gmail.com>
2014-10-08 16:49:51 -0700

    Played with the compiler output messages a bit, to make it more readable

--- make.sh
+++ make.sh
@@ -1,9 +1,11 @@
 #!/bin/tcsh
 ###############################################################################
 
+rm ./bin/essi
+
 # building supporting libraries
 # 16 CPUs 
-cd nDarray          ;    time make -j 16   ;   cd ..
+cd nDarray         	 				 ;    time make -j 16   ;   cd ..
 cd ModifiedOpenSeesServices          ;    time make -j 16   ;   cd ..
 cd CompGeoMechUCD_MaterialModeling   ;    time make -j 16   ;   cd ..
 cd CompGeoMechUCD_FiniteElements     ;    time make -j 16   ;   cd ..
@@ -11,4 +13,21 @@ cd CompGeoMechUCD_Miscellaneous      ;    time make -j 16   ;   cd ..
 cd small_utility_programs            ;    time make -j 16   ;   cd ..
 cd UCB_Elements                      ;    time make -j 16   ;   cd ..
 cd DSL                               ;    time make         ;   cd ..
+
+echo ""
+
+echo "Contents of "$PWD"/lib"
 dir lib
+
+echo ""
+echo ""
+
+set essiexecutable = $PWD/bin/essi
+if ( -e $essiexecutable) then
+	echo "Compilation successful! "
+	echo "ESSI exccutable can be found in "$PWD"/bin"
+else
+	echo "Compilation FAILED."
+endif 
+
+
