28e5480d63bd0825c36593a84fe1a47053125e25
Jose Abell <ja.abell@gmail.com>
2015-06-29 15:17:59 -0700

    removing send/recv self from petscsolver

--- ModifiedOpenSeesServices/system_of_eqn/linearSOE/petsc/PetscSolver.cpp
+++ ModifiedOpenSeesServices/system_of_eqn/linearSOE/petsc/PetscSolver.cpp
@@ -340,151 +340,152 @@ PetscSolver::setLinearSOE(PetscSOE &theSys)
 int
 PetscSolver::sendSelf(int cTag, Channel &theChannel)
 {
-    static ID idData(4);
-    idData(0) = maxIts;
-    //if (method == KSPCG)
-    //  idData(1) = 0;
-    //else if (method == KSPBICG)
-    //  idData(1) = 1;
-    //else if (method == KSPRICHARDSON)
-    //  idData(1) = 2;
-    //else if (method == KSPCHEBYCHEV)
-    //  idData(1) = 3;
-    //else if (method == KSPGMRES)
-    //  idData(1) = 4;
-    //else if (method == KSPPREONLY)
-    //  idData(1) = 5;
-    //else {
-    //  cerr << "PetscSolver::sendSelf() - unknown method set\n";
-    //  return -1;
-    //}
-    //
-    //if (preconditioner == PCJACOBI)
-    //  idData(2) = 0;
-    //else if (preconditioner == PCILU)
-    //  idData(2) = 1;
-    //else if (preconditioner == PCICC)
-    //  idData(2) = 2;
-    //else if (preconditioner == PCBJACOBI)
-    //  idData(2) = 3;
-    //else if (preconditioner == PCNONE)
-    //  idData(2) = 4;
-    //else if (preconditioner == PCLU)
-    //  idData(2) = 5;
-    //else if (preconditioner == PCCHOLESKY)
-    //  idData(2) = 6;
-    //else {
-    //  cerr << "PetscSolver::sendSelf() - unknown preconditioner set\n";
-    //  return -1;
-    //}
+    cout << "PetscSolver::sendSelf()\n";
+    // static ID idData(4);
+    // idData(0) = maxIts;
+    // //if (method == KSPCG)
+    // //  idData(1) = 0;
+    // //else if (method == KSPBICG)
+    // //  idData(1) = 1;
+    // //else if (method == KSPRICHARDSON)
+    // //  idData(1) = 2;
+    // //else if (method == KSPCHEBYCHEV)
+    // //  idData(1) = 3;
+    // //else if (method == KSPGMRES)
+    // //  idData(1) = 4;
+    // //else if (method == KSPPREONLY)
+    // //  idData(1) = 5;
+    // //else {
+    // //  cerr << "PetscSolver::sendSelf() - unknown method set\n";
+    // //  return -1;
+    // //}
+    // //
+    // //if (preconditioner == PCJACOBI)
+    // //  idData(2) = 0;
+    // //else if (preconditioner == PCILU)
+    // //  idData(2) = 1;
+    // //else if (preconditioner == PCICC)
+    // //  idData(2) = 2;
+    // //else if (preconditioner == PCBJACOBI)
+    // //  idData(2) = 3;
+    // //else if (preconditioner == PCNONE)
+    // //  idData(2) = 4;
+    // //else if (preconditioner == PCLU)
+    // //  idData(2) = 5;
+    // //else if (preconditioner == PCCHOLESKY)
+    // //  idData(2) = 6;
+    // //else {
+    // //  cerr << "PetscSolver::sendSelf() - unknown preconditioner set\n";
+    // //  return -1;
+    // //}
 
-    if (strcmp(method, "cg") == 0)
-    {
-        idData(1) = 0;
-    }
-    else if (strcmp(method, "bicg") == 0)
-    {
-        idData(1) = 1;
-    }
-    else if (strcmp(method, "richardson") == 0)
-    {
-        idData(1) = 2;
-    }
-    else if (strcmp(method, "chebychev") == 0)
-    {
-        idData(1) = 3;
-    }
-    else if (strcmp(method, "gmres") == 0)
-    {
-        idData(1) = 4;
-    }
-    else if (strcmp(method, "preonly") == 0)
-    {
-        idData(1) = 5;
-    }
-    else
-    {
-        cerr << "PetscSolver::sendSelf() - unknown method set\n";
-        return -1;
-    }
+    // if (strcmp(method, "cg") == 0)
+    // {
+    //     idData(1) = 0;
+    // }
+    // else if (strcmp(method, "bicg") == 0)
+    // {
+    //     idData(1) = 1;
+    // }
+    // else if (strcmp(method, "richardson") == 0)
+    // {
+    //     idData(1) = 2;
+    // }
+    // else if (strcmp(method, "chebychev") == 0)
+    // {
+    //     idData(1) = 3;
+    // }
+    // else if (strcmp(method, "gmres") == 0)
+    // {
+    //     idData(1) = 4;
+    // }
+    // else if (strcmp(method, "preonly") == 0)
+    // {
+    //     idData(1) = 5;
+    // }
+    // else
+    // {
+    //     cerr << "PetscSolver::sendSelf() - unknown method set\n";
+    //     return -1;
+    // }
 
-    if (strcmp(preconditioner, "jacobi") == 0)
-    {
-        idData(2) = 0;
-    }
-    else if (strcmp(preconditioner, "ilu") == 0)
-    {
-        idData(2) = 1;
-    }
-    else if (strcmp(preconditioner, "icc") == 0)
-    {
-        idData(2) = 2;
-    }
-    else if (strcmp(preconditioner, "bjacobi") == 0)
-    {
-        idData(2) = 3;
-    }
-    else if (strcmp(preconditioner, "none") == 0)
-    {
-        idData(2) = 4;
-    }
-    else if (strcmp(preconditioner, "lu") == 0)
-    {
-        idData(2) = 5;
-    }
-    else if (strcmp(preconditioner, "cholesky") == 0)
-    {
-        idData(2) = 6;
-    }
-    else
-    {
-        cerr << "PetscSolver::sendSelf() - unknown preconditioner set\n";
-        return -1;
-    }
+    // if (strcmp(preconditioner, "jacobi") == 0)
+    // {
+    //     idData(2) = 0;
+    // }
+    // else if (strcmp(preconditioner, "ilu") == 0)
+    // {
+    //     idData(2) = 1;
+    // }
+    // else if (strcmp(preconditioner, "icc") == 0)
+    // {
+    //     idData(2) = 2;
+    // }
+    // else if (strcmp(preconditioner, "bjacobi") == 0)
+    // {
+    //     idData(2) = 3;
+    // }
+    // else if (strcmp(preconditioner, "none") == 0)
+    // {
+    //     idData(2) = 4;
+    // }
+    // else if (strcmp(preconditioner, "lu") == 0)
+    // {
+    //     idData(2) = 5;
+    // }
+    // else if (strcmp(preconditioner, "cholesky") == 0)
+    // {
+    //     idData(2) = 6;
+    // }
+    // else
+    // {
+    //     cerr << "PetscSolver::sendSelf() - unknown preconditioner set\n";
+    //     return -1;
+    // }
 
-    //if (matType == MATSUPERLU_DIST)
-    //  idData(3) = 0;
-    //else if (matType == MATMPISBAIJSPOOLES)
-    //  idData(3) = 1;
-    //else if (matType == MATMPIROWBS)
-    //  idData(3) = 2;
-    if (strcmp(matType, "superlu_dist") == 0)
-    {
-        idData(3) = 0;
-    }
-    else if (strcmp(matType, "mpisbaijspooles") == 0)
-    {
-        idData(3) = 1;
-    }
-    else if (strcmp(matType, "mpirowbs") == 0)
-    {
-        idData(3) = 2;
-    }
-    else if (strcmp(matType, "mpiaijspooles") == 0)
-    {
-        idData(3) = 3;
-    }
-    else if (strcmp(matType, "aijmumps") == 0)
-    {
-        idData(3) = 4;
-    }
-    else
-    {
-        idData(3) = 99;
-        //  cerr << "PetscSolver::sendSelf() - unknown matType set\n";
-        //  return -1;
-    }
+    // //if (matType == MATSUPERLU_DIST)
+    // //  idData(3) = 0;
+    // //else if (matType == MATMPISBAIJSPOOLES)
+    // //  idData(3) = 1;
+    // //else if (matType == MATMPIROWBS)
+    // //  idData(3) = 2;
+    // if (strcmp(matType, "superlu_dist") == 0)
+    // {
+    //     idData(3) = 0;
+    // }
+    // else if (strcmp(matType, "mpisbaijspooles") == 0)
+    // {
+    //     idData(3) = 1;
+    // }
+    // else if (strcmp(matType, "mpirowbs") == 0)
+    // {
+    //     idData(3) = 2;
+    // }
+    // else if (strcmp(matType, "mpiaijspooles") == 0)
+    // {
+    //     idData(3) = 3;
+    // }
+    // else if (strcmp(matType, "aijmumps") == 0)
+    // {
+    //     idData(3) = 4;
+    // }
+    // else
+    // {
+    //     idData(3) = 99;
+    //     //  cerr << "PetscSolver::sendSelf() - unknown matType set\n";
+    //     //  return -1;
+    // }
 
 
 
-    theChannel.sendID(0, cTag, idData);
+    // theChannel.sendID(0, cTag, idData);
 
-    static Vector data(3);
-    data(0) = rTol;
-    data(1) = aTol;
-    data(2) = dTol;
+    // static Vector data(3);
+    // data(0) = rTol;
+    // data(1) = aTol;
+    // data(2) = dTol;
 
-    theChannel.sendVector(0, cTag, data);
+    // theChannel.sendVector(0, cTag, data);
     return 0;
 }
 
@@ -492,110 +493,111 @@ int
 PetscSolver::receiveSelf(int cTag, Channel &theChannel,
                          FEM_ObjectBroker &theBroker)
 {
-    static ID idData(4);
-    theChannel.receiveID(0, cTag, idData);
-    maxIts = idData(0);
+    cout << "PetscSolver::receiveSelf() \n";
+    // static ID idData(4);
+    // theChannel.receiveID(0, cTag, idData);
+    // maxIts = idData(0);
 
-    if (idData(1) == 0)
-    {
-        method = KSPCG;
-    }
-    else if (idData(1) == 1)
-    {
-        method = KSPBICG;
-    }
-    else if (idData(1) == 2)
-    {
-        method = KSPRICHARDSON;
-    }
-    else if (idData(1) == 3)
-    {
-        method = KSPCHEBYSHEV;
-    }
-    else if (idData(1) == 4)
-    {
-        method = KSPGMRES;
-    }
-    else if (idData(1) == 5)
-    {
-        method = KSPPREONLY;
-    }
-    else
-    {
-        cerr << "PetscSolver::receiveSelf() - unknown method recvd\n";
-        return -1;
-    }
+    // if (idData(1) == 0)
+    // {
+    //     method = KSPCG;
+    // }
+    // else if (idData(1) == 1)
+    // {
+    //     method = KSPBICG;
+    // }
+    // else if (idData(1) == 2)
+    // {
+    //     method = KSPRICHARDSON;
+    // }
+    // else if (idData(1) == 3)
+    // {
+    //     method = KSPCHEBYSHEV;
+    // }
+    // else if (idData(1) == 4)
+    // {
+    //     method = KSPGMRES;
+    // }
+    // else if (idData(1) == 5)
+    // {
+    //     method = KSPPREONLY;
+    // }
+    // else
+    // {
+    //     cerr << "PetscSolver::receiveSelf() - unknown method recvd\n";
+    //     return -1;
+    // }
 
-    if (idData(2) == 0)
-    {
-        preconditioner = PCJACOBI;
-    }
-    else if (idData(2) == 1)
-    {
-        preconditioner = PCILU;
-    }
-    else if (idData(2) == 2)
-    {
-        preconditioner = PCICC;
-    }
-    else if (idData(2) == 3)
-    {
-        preconditioner = PCBJACOBI;
-    }
-    else if (idData(2) == 4)
-    {
-        preconditioner = PCNONE;
-    }
-    else if (idData(2) == 5)
-    {
-        preconditioner = PCLU;
-    }
-    else if (idData(2) == 6)
-    {
-        preconditioner = PCCHOLESKY;
-    }
-    else
-    {
-        cerr << "PetscSolver::receiveSelf() - unknown preconditioner set\n";
-        return -1;
-    }
+    // if (idData(2) == 0)
+    // {
+    //     preconditioner = PCJACOBI;
+    // }
+    // else if (idData(2) == 1)
+    // {
+    //     preconditioner = PCILU;
+    // }
+    // else if (idData(2) == 2)
+    // {
+    //     preconditioner = PCICC;
+    // }
+    // else if (idData(2) == 3)
+    // {
+    //     preconditioner = PCBJACOBI;
+    // }
+    // else if (idData(2) == 4)
+    // {
+    //     preconditioner = PCNONE;
+    // }
+    // else if (idData(2) == 5)
+    // {
+    //     preconditioner = PCLU;
+    // }
+    // else if (idData(2) == 6)
+    // {
+    //     preconditioner = PCCHOLESKY;
+    // }
+    // else
+    // {
+    //     cerr << "PetscSolver::receiveSelf() - unknown preconditioner set\n";
+    //     return -1;
+    // }
 
-    if (idData(3) == 0)
-        //matType = MATSUPERLU_DIST; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
-    {
-        matType = MATSOLVERSUPERLU_DIST;
-    }
-    else if (idData(3) == 1)
-        //matType = MATMPISBAIJSPOOLES; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
-    {
-        matType = -1;//MATSOLVERSPOOLES;
-    }
-    //else if (idData(3) == 2)
-    //matType = MATMPIROWBS;  Ignored because this interface has been removed fro petsc 3.1   by B. Kamrani (Aug. 2010)
-    else if (idData(3) == 3)
-        //matType = MATMPIAIJSPOOLES; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
-    {
-        matType = -1;//MATSOLVERSPOOLES;
-    }
-    else if (idData(3) == 4)
-        //matType = MATAIJMUMPS; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
-    {
-        matType = MATSOLVERMUMPS;
-    }
-    else
-    {
-        matType = MATMPIAIJ;
-        //  cerr << "PetscSolver::receiveSelf() - unknown matType set\n";
-        //  return -1;
-    }
+    // if (idData(3) == 0)
+    //     //matType = MATSUPERLU_DIST; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
+    // {
+    //     matType = MATSOLVERSUPERLU_DIST;
+    // }
+    // else if (idData(3) == 1)
+    //     //matType = MATMPISBAIJSPOOLES; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
+    // {
+    //     matType = MATSOLVERSUPERLU_DIST;//MATSOLVERSPOOLES;
+    // }
+    // //else if (idData(3) == 2)
+    // //matType = MATMPIROWBS;  Ignored because this interface has been removed fro petsc 3.1   by B. Kamrani (Aug. 2010)
+    // else if (idData(3) == 3)
+    //     //matType = MATMPIAIJSPOOLES; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
+    // {
+    //     matType = MATSOLVERSUPERLU_DIST;//MATSOLVERSPOOLES;
+    // }
+    // else if (idData(3) == 4)
+    //     //matType = MATAIJMUMPS; Changed because this interface has been changed in  petsc 3.1   by B. Kamrani (Aug. 2010)
+    // {
+    //     matType = MATSOLVERMUMPS;
+    // }
+    // else
+    // {
+    //     matType = MATMPIAIJ;
+    //     //  cerr << "PetscSolver::receiveSelf() - unknown matType set\n";
+    //     //  return -1;
+    // }
 
-    static Vector data(3);
-    theChannel.receiveVector(0, cTag, data);
-    rTol = data(0);
-    aTol = data(1);
-    dTol = data(2);
+    // static Vector data(3);
+    // theChannel.receiveVector(0, cTag, data);
+    // rTol = data(0);
+    // aTol = data(1);
+    // dTol = data(2);
 
-    theSOE->mType = matType;
+    // theSOE->mType = matType;
 
     return 0;
 }
