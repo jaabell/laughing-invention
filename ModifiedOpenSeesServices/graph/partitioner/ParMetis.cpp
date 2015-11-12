b2436d3df5ee5908934c71f61e822a8f22d5bbb4
Jose Abell <jaabell@ucdavis.edu>
2015-11-12 15:32:49 -0800

    Also enabled repartitioning:

--- ModifiedOpenSeesServices/graph/partitioner/ParMetis.cpp
+++ ModifiedOpenSeesServices/graph/partitioner/ParMetis.cpp
@@ -303,7 +303,7 @@ ParMetis::partition(Graph *theGraph, int numPart)
 int
 ParMetis::repartition(Graph *theGraph, int numPart)
 {
-    return 0;
+    // return 0;
 # ifdef _PDD
     int npes, mype;
     int nvtxs;
