4d16cbd20283338286842aca8969d13b18c8a64d
Jose Abell <jaabell@ucdavis.edu>
2016-02-02 23:32:24 -0800

    ...

--- CompGeoMechUCD_MaterialModeling/ClassicElastoplasticMaterial/ClassicElastoplasticMaterial.h
+++ CompGeoMechUCD_MaterialModeling/ClassicElastoplasticMaterial/ClassicElastoplasticMaterial.h
@@ -607,23 +607,23 @@ private:
 
         DTensor4& Eelastic = et(sigma);
 
-        // for (int ii = 0; ii < 3; ii++)
-        // {
-        //     for (int jj = 0; jj < 3; jj++)
-        //     {
-        //         for (int kk = 0; kk < 3; kk++)
-        //         {
-        //             for (int ll = 0; ll < 3; ll++)
-        //             {
-        //                 dsigma(ii, jj) += Eelastic(ii, jj, kk, ll) * depsilon(kk, ll);
-        //             }
-        //         }
-        //         TrialStress(ii, jj) = sigma(ii, jj) + dsigma(ii, jj);
-        //         TrialStrain(ii, jj) = CommitStrain(ii, jj) + depsilon(ii, jj);
-        //         TrialPlastic_Strain(ii, jj) = CommitPlastic_Strain(ii, jj);
-        //     }
-        // }
-        dsigma(i, j) = Eelastic(i, j, k, l) * depsilon(k, l);
+        for (int ii = 0; ii < 3; ii++)
+        {
+            for (int jj = 0; jj < 3; jj++)
+            {
+                for (int kk = 0; kk < 3; kk++)
+                {
+                    for (int ll = 0; ll < 3; ll++)
+                    {
+                        dsigma(ii, jj) += Eelastic(ii, jj, kk, ll) * depsilon(kk, ll);
+                    }
+                }
+                // TrialStress(ii, jj) = sigma(ii, jj) + dsigma(ii, jj);
+                // TrialStrain(ii, jj) = CommitStrain(ii, jj) + depsilon(ii, jj);
+                // TrialPlastic_Strain(ii, jj) = CommitPlastic_Strain(ii, jj);
+            }
+        }
+        // dsigma(i, j) = Eelastic(i, j, k, l) * depsilon(k, l);
         TrialStress(i, j) = sigma(i, j) + dsigma(i, j);
         TrialStrain(i, j) = CommitStrain(i, j) + depsilon(i, j);
         TrialPlastic_Strain(i, j) = CommitPlastic_Strain(i, j);
