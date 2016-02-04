5c3adaeb69b0fb99c728f48cb3f420743eb60c68
Jose Abell <jaabell@ucdavis.edu>
2016-02-03 16:48:49 -0800

    Added traits class

---
+++ CompGeoMechUCD_MaterialModeling/ClassicElastoplasticMaterial/CEPTraits.h
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////
+//   COPYLEFT (C): Woody's viral GPL-like license (by BJ):
+//                 ``This    source  code is Copyrighted in
+//                 U.S.,  for  an  indefinite  period,  and anybody
+//                 caught  using it without our permission, will be
+//                 mighty good friends of ourn, cause we don't give
+//                 a  darn.  Hack it. Compile it. Debug it. Run it.
+//                 Yodel  it.  Enjoy it. We wrote it, that's all we
+//                 wanted to do.''
+//
+//
+// COPYRIGHT (C):     :-))
+// PROJECT:           Object Oriented Finite Element Program
+// FILE:
+// CLASS:
+// MEMBER FUNCTIONS:
+//
+// MEMBER VARIABLES
+//
+// PURPOSE:
+//
+// RETURN:
+// VERSION:
+// LANGUAGE:          C++
+// TARGET OS:
+// PROGRAMMER:
+// DATE:
+// UPDATE HISTORY:
+//
+//
+/////////////////////////////////////////////////////////////////////////////
+
+//Some basic trait classes to aid with implementation of CEP
+
+template< typename T >
+struct supports_pre_integration_callback
+{
+    static const bool value = false;
+};
+
+template< typename T >
+struct supports_post_integration_callback
+{
+    static const bool value = false;
+};
+
+template< typename T >
+struct supports_implicit_integration
+{
+    static const bool value = false;
+};
+
