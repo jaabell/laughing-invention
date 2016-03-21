691bb4c303f92e1c936be8029726abf014c6d19a
Jose Abell <jaabell@ucdavis.edu>
2016-03-21 13:56:30 -0700

    ...

--- CMake/Modules/essi.cmake
+++ CMake/Modules/essi.cmake
@@ -66,17 +66,17 @@ ENDMACRO()
 # Macro to add a single external library
 MACRO(IMPORT_NATIVE_LIB lib_dir lib_name alt_dir)
     FIND_PACKAGE(${lib_name})
-    message(STATUS ${HDF5_LIBRARY_DIRS})
-    message(STATUS ${HDF5_INCLUDE_DIRS}) # - Location of the hdf5 includes
-    message(STATUS ${HDF5_INCLUDE_DIR}) # - Location of the hdf5 includes (deprecated)
-    message(STATUS ${HDF5_DEFINITIONS}) # - Required compiler definitions for HDF5
-    message(STATUS ${HDF5_C_LIBRARIES}) # - Required libraries for the HDF5 C bindings.
-    message(STATUS ${HDF5_CXX_LIBRARIES}) # - Required libraries for the HDF5 C++ bindings
-    message(STATUS ${HDF5_Fortran_LIBRARIES}) # - Required libraries for the HDF5 Fortran bindings
-    message(STATUS ${HDF5_HL_LIBRARIES}) # - Required libraries for the HDF5 high level API
-    message(STATUS ${HDF5_Fortran_HL_LIBRARIES}) # - Required libraries for the high level Fortran                            bindings.
-    message(STATUS ${HDF5_LIBRARIES}) # - Required libraries for all requested bindings
-    message(STATUS ${HDF5_FOUND}) # - true if HDF5 was found on the system
+    message(STATUS "HDF5_LIBRARY_DIRS = ${HDF5_LIBRARY_DIRS}")
+    message(STATUS "HDF5_INCLUDE_DIRS = ${HDF5_INCLUDE_DIRS}") # - Location of the hdf5 includes
+    message(STATUS "HDF5_INCLUDE_DIR = ${HDF5_INCLUDE_DIR}") # - Location of the hdf5 includes (deprecated)
+    message(STATUS "HDF5_DEFINITIONS = ${HDF5_DEFINITIONS}") # - Required compiler definitions for HDF5
+    message(STATUS "HDF5_C_LIBRARIES = ${HDF5_C_LIBRARIES}") # - Required libraries for the HDF5 C bindings.
+    message(STATUS "HDF5_CXX_LIBRARIES = ${HDF5_CXX_LIBRARIES}") # - Required libraries for the HDF5 C++ bindings
+    message(STATUS "HDF5_Fortran_LIBRARIES = ${HDF5_Fortran_LIBRARIES}") # - Required libraries for the HDF5 Fortran bindings
+    message(STATUS "HDF5_HL_LIBRARIES = ${HDF5_HL_LIBRARIES}") # - Required libraries for the HDF5 high level API
+    message(STATUS "HDF5_Fortran_HL_LIBRARIES = ${HDF5_Fortran_HL_LIBRARIES}") # - Required libraries for the high level Fortran                            bindings.
+    message(STATUS "HDF5_LIBRARIES = ${HDF5_LIBRARIES}") # - Required libraries for all requested bindings
+    message(STATUS "HDF5_FOUND = ${HDF5_FOUND}") # - true if HDF5 was found on the system
     if(${lib_name}_FOUND)
         message(STATUS ${${lib_name}_LIBRARIES})
         add_library(${${lib_name}}_LIBRARIES SHARED IMPORTED)
